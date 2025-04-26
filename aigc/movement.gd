5f8fcad4abd034b46b3bc9aba4195e4f0d9dd2a1
snjfhmx <snickersbar13@comcast.net>
2025-04-26 16:22:19 -0600

    Camera & Movement

--- aigc/movement.gd
+++ aigc/movement.gd
@@ -1,19 +1,19 @@
 extends CharacterBody2D
 
-var speed = 250
+var speed = 500
 
-func _physics_process(_delta):
+func _process(delta):
 	var motion = Vector2()
-
-	if Input.is_action_pressed("KEY_A"):
+	if Input.is_key_pressed(KEY_A):
 		motion.x -= 1
-	if Input.is_action_pressed("KEY_D"):
+	if Input.is_key_pressed(KEY_D):
 		motion.x += 1
-	if Input.is_action_pressed("KEY_W"):
+	if Input.is_key_pressed(KEY_W):
 		motion.y -= 1
-	if Input.is_action_pressed("KEY_S"):
+	if Input.is_key_pressed(KEY_S):
 		motion.y += 1
-
-	motion = motion.normalized() * speed
+#.normalized()
+	motion = motion * speed
 	velocity = motion  # Set the velocity property
 	move_and_slide()   # This now uses the built-in velocity
+	
