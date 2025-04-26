d9469249c33af317ddc5dcde7a3bcf45a2a3043c
snjfhmx <snickersbar13@comcast.net>
2025-04-26 15:01:42 -0600

    Movement
    Movement

--- aigc/movement.gd
+++ aigc/movement.gd
@@ -1,11 +1,19 @@
 extends CharacterBody2D
-var speedMult = 300
-var delta = 1
-func _ready():
-	_process(delta)
 
-func _process(delta):
-	#push_error("Bruh2")
-	if Input.is_physical_key_pressed(KEY_A):
-		push_error("Bruh")
-	
+var speed = 250
+
+func _physics_process(_delta):
+	var motion = Vector2()
+
+	if Input.is_action_pressed("Left"):
+		motion.x -= 1
+	if Input.is_action_pressed("Right"):
+		motion.x += 1
+	if Input.is_action_pressed("Up"):
+		motion.y -= 1
+	if Input.is_action_pressed("Down"):
+		motion.y += 1
+
+	motion = motion.normalized() * speed
+	velocity = motion  # Set the velocity property
+	move_and_slide()   # This now uses the built-in velocity
